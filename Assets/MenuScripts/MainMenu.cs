5c2ff7d185fefb6f9a6786dcdc47c7a5709cafcf
novvedsama <novvedsama@gmail.com>
2025-05-07 23:49:43 -0700

    update

--- Assets/MenuScripts/MainMenu.cs
+++ Assets/MenuScripts/MainMenu.cs
@@ -3,29 +3,20 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-<<<<<<< HEAD
-    public void GoToGame(string Scene)
-    {
-        SceneManager.LoadScene(2);
-    }
-    public void GoToCredits(string Scene)
-    {
-        SceneManager.LoadScene(2);
-=======
-    [SerializeField] private GameObject sceneLoader;
-    [SerializeField] private int sceneIndex;
-
-    public void GoToScene()
-    {
-        sceneLoader.GetComponent<SceneLoader>().LoadNextScene(sceneIndex);
-    }
-
     public void GotoMainMenu()
     {
-        sceneLoader.GetComponent<SceneLoader>().LoadNextScene(0);
->>>>>>> 0ea0562a078d345f377d23d22b9b1339bfc94e4b
+        SceneManager.LoadScene(1);
+
+    }
+    public void GoToGame()
+    {
+        SceneManager.LoadScene(2);
     }
 
+    public void GoToCredits()
+    {
+        SceneManager.LoadScene(2);
+    }
     public void QuitApp()
     {
         Application.Quit();
